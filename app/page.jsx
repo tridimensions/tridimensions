066273930ecb924c58897de738ed8414a8b57905
tridimensions <stephane@tridimensions.ca>
2026-03-04 19:25:50 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,8 @@
-import StripeCart from './shop/page';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const StripeCart = dynamic(() => import('./shop/page'), { ssr: false });
 
 export default function Home() {
   return <StripeCart />;
